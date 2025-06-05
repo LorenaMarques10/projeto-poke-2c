963ae8d7569866d7d970be2be46a141f0505ece9
LorenaMarques10 <rm94314@estudante.fieb.edu.br>
2025-06-05 10:48:24 +0000

    Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route} from "react-router-dom";
 import Home from './Home'
 import PokeFlex from './PokeFlex'
 import PokeGrid from './PokeGrid'
-
-
-
+import Menu from'./Menu'
+ 
 const App = () => {
-
+ 
   return (
     <Routes>
       <Route path = "/" element={<Home/>} />
-      <Routes path ="PokeFlex" element={<PokeFlex/>}/>
-      <Routes path ="PokeFlex" element={<PokeGrind/>}/>
-
-
+      <Route path = "pokeflex" element={<PokeFlex/>} />
+      <Route path = "pokegrid" element={<PokeGrid/>} />
     </Routes>
   )
 }
-
+ 
 export default App
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,17 @@
 import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg'
 import './Home.css'
-import PokeGrid from'./PokeGrid'
-import PokeFlex from'./PokeFlex'
+import PokeGrid from './PokeGrid'
+import PokeFlex from './PokeFlex'
+ 
 const Home = () => {
-
+ 
     return (
         <div>
-            <PokeGrid/>  
-            <PokeFlex/>   
+            <PokeGrid />
+            <PokeFlex />
         </div>
     )
 }
-
+ 
 export default Home
