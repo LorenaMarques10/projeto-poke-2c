47bf808df7db3b902cfc6a31f75507adac68a8ee
LorenaMarques10 <rm94314@estudante.fieb.edu.br>
2025-06-05 11:23:30 +0000

    menu

--- src/PokeFlex.jsx
+++ src/PokeFlex.jsx
@@ -1,10 +1,18 @@
 import poke from './assets/pokeapi_256.png'
 import eevee from './assets/eevee.jpg'
 import './PokeFlex.css'
+import Menu from './Menu'
  
 const PokeFlex = () => {
  
     return (
+        
+<div> 
+    <Menu/>
+
+
+
+
         <div className="PokeFlex">
             <img src={poke}/>
             <img src={eevee}/>
@@ -18,6 +26,7 @@ const PokeFlex = () => {
             <img src={eevee}/>
             <img src={poke}/>
             <img src={eevee}/>
+        </div>
         </div>
     )
 }
